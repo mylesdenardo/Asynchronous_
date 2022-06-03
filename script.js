6e8c2c55dd27b4783d7272439ac2fca8581629d9
mylesdenardo <themylesd@gmail.com>
2022-06-03 14:19:58 -0400

    challenge 1

--- script.js
+++ script.js
@@ -42,21 +42,38 @@ getCountryData('germany');
 const renderCountry = function (data, className = '') {
   const html = `
   <article class="country ${className}">
-      <img class="country__img" src="${data.flag}" />
+      <img class="country__img" src="${data.flags.svg}" />
       <div class="country__data">
-        <h3 class="country__name">${data.name}</h3>
+        <h3 class="country__name">${data.name.common}</h3>
         <h4 class="country__region">${data.region}</h4>
         <p class="country__row"><span>👫</span>${(
           +data.population / 1000000
         ).toFixed(1)} people</p>
-        <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p> 
-        <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+        <p class="country__row"><span>🗣️</span>${
+          Object.values(data.languages)[0]
+        }</p> 
+        <p class="country__row"><span>💰</span>${
+          Object.values(data.currencies)[0].name
+        }</p>
       </div>
     </article>
   `;
 
   countriesContainer.insertAdjacentHTML('beforeend', html);
   countriesContainer.style.opacity = 1;
+};
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  countriesContainer.style.opacity = 1;
+};
+
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+
+    return response.json();
+  });
 };
 
 /*
@@ -103,10 +120,47 @@ setTimeout(() => {
 }, 1000);
 
 */
-
+/*
 const getCountryData = function (country) {
-  fetch(`https://restcountries.eu/rest/v3.3/name/${country}`)
-    .then(response => response.json())
-    .then(data => renderCountry(data[0]));
+  getJSON(`https://restcountries.com/v3.1/name/${country}`).then(data => {
+    renderCountry(data[0]);
+    console.log(data);
+    const [neighbour] = data[0].borders;
+    if (!neighbour) throw new Error('No neighbour found!');
+
+    return getJSON(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+  });
 };
+
 getCountryData('usa');
+*/
+
+/////////////////////////////////////////////////////////
+// Challenge 1
+
+const whereAmI = function (lat, lng) {
+  fetch(
+    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}&localityLanguage=en`
+  )
+    .then(res => {
+      if (!res.ok) throw new Error(`Problem with geocoding ${res.status}`);
+      return res.json();
+    })
+    .then(data => {
+      console.log(data);
+      console.log(`You are in ${data.city}, ${data.countryName}`);
+
+      return fetch(`https://restcountries.eu/v3.1/name/${data.countryName}`);
+    })
+    .then(response => {
+      if (!response.ok)
+        throw new Error(`Country not found (${response.status})`);
+
+      return response.json();
+    })
+    .then(data => renderCountry(data))
+    .catch(err => console.error(`${err.message} 💥`));
+};
+
+whereAmI(52.502, 13.212);
+whereAmI(19.037, 72.873);
