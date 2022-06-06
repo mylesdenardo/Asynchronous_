87015316df463f3c52fba66896a1f0f3dfdb8d36
mylesdenardo <themylesd@gmail.com>
2022-06-06 12:43:27 -0400

    Building a single promise

--- script.js
+++ script.js
@@ -166,3 +166,53 @@ const whereAmI = function (lat, lng) {
 whereAmI(52.502, 13.212);
 whereAmI(19.037, 72.873);
 */
+
+/*
+//////////////////////////////////////////////////////////////////
+// Event Loop Practice
+
+console.log('Test start');
+setTimeout(() => console.log('0 second timer'), 0);
+Promise.resolve('Resolved promise 1').then(res => console.log(res));
+
+Promise.resolve('Resolved promise 2').then(res => {
+  for (let i = 0; i < 1000000; i++) {}
+  console.log(res);
+});
+
+console.log('Test end');
+
+// ^^ Microtasks queue will be executed before a callback queue
+*/
+
+//encapsulating asynchronous behavior in a promise
+
+const lotteryPromise = new Promise(function (resolve, reject) {
+  console.log('Lottery draw is happening 🔮');
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve('You win! 💰');
+    } else {
+      reject(new Error('You lost! 💩'));
+    }
+  }, 2000);
+});
+
+lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+// Promisifying setTimeout
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+wait(2)
+  .then(() => {
+    console.log('I waited for 2 seconds');
+    return wait(1);
+  })
+  .then(() => console.log('I wated for 1 second'));
+
+Promise.resolve('abc').then(x => console.log(x));
+Promise.reject(new Error('Problem!')).catch(x => console.log(x));
